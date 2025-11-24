23e64b4a78b1ec496075f544d64aeec545d9dae0
krishna paralkar <krishnaparalkar41@gmail.com>
2025-11-25 00:05:50 +0530

    Add files via upload

---
+++ C/IDE2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int i;
+    printf("Ascending order\n");
+    for(int i = 0;i<=10;i++);{
+    printf("%d", i);
+    }
+    printf("Descending order\n");
+    for(int i = 0;i > 0; i--);{
+    printf("%d", i);
+    }
+    return 0;
+
+}
